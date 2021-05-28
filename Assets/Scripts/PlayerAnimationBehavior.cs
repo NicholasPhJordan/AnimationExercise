f660fb0c789fc4a14f52f79ce3d93f040d6eed08
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-05-28 11:58:09 -0500

    Added animation

---
+++ Assets/Scripts/PlayerAnimationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationBehavior : MonoBehaviour
+{
+    private PlayerMovementBehaviour _movementBehavior;
+    [SerializeField]
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movementBehavior = GetComponent<PlayerMovementBehaviour>();
+    }
+
+    public void ActivateWinTrigger()
+    {
+        _animator.SetTrigger("WinGame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Speed", _movementBehavior.Velocity.magnitude);
+        _animator.SetBool("InAir", !_movementBehavior.IsGrounded);
+    }
+}
